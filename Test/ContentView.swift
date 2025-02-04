7c40e3598912bf655e00fca8c84b6e7bf847bf14
Thomas Versteeg <t.versteeg2@student.avans.nl>
2025-02-04 15:33:49 +0100

    Initial Commit

---
+++ Test/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by Thomas Versteeg on 04/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Thomas!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Thomas Versteeg on 04/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
